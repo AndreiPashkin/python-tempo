38dd2095923d5bfc784ee1bc5163d92818a6344d
Andrew Pashkin <andrew.pashkin@gmx.co.uk>
2015-06-21 01:52:55 +0300

    Tuned setup.py to install static files

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ setup(
     author_email="andrew.pashkin@gmx.co.uk",
     license="BSD",
     packages=find_packages(include=['tempo', 'tempo.*']),
+    package_data={
+        'tempo.django': ['static/tempo/*.css', 'static/tempo/*.js']
+    },
     install_requires=[line.strip() for line in open("requirements.txt")],
     dependency_links=[line.strip() for line in open("dependency-links.txt")],
 )
