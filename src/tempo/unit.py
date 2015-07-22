17cc4bcae2542cfad34d79ec49f7a2ea04de254b
Andrew Pashkin <andrew.pashkin@gmx.co.uk>
2015-07-22 10:19:46 +0300

    Move delta and floor fns to separate module

---
+++ src/tempo/unit.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding=utf-8
+SECONDS_IN_MINUTE = 60
+MINUTES_IN_HOUR = 60
+SECONDS_IN_HOUR = SECONDS_IN_MINUTE * MINUTES_IN_HOUR
+HOURS_IN_DAY = 24
+MINUTES_IN_DAY = MINUTES_IN_HOUR * HOURS_IN_DAY
+SECONDS_IN_DAY = MINUTES_IN_DAY * SECONDS_IN_MINUTE
+DAYS_IN_WEEK = 7
+HOURS_IN_WEEK = HOURS_IN_DAY * DAYS_IN_WEEK
+MINUTES_IN_WEEK = HOURS_IN_WEEK * MINUTES_IN_HOUR
+SECONDS_IN_WEEK = MINUTES_IN_WEEK * SECONDS_IN_MINUTE
+MONTHS_IN_YEAR = 12
+
+
+class Unit:
+    """"Enumeration of supported time units."""
+    SECOND  = 'second'
+    MINUTE  = 'minute'
+    HOUR    = 'hour'
+    DAY     = 'day'
+    WEEK    = 'week'
+    MONTH   = 'month'
+    YEAR    = 'year'
+
+
+# Order of places in time representation
+UNIT_ORDER = {
+    Unit.SECOND: 1,
+    Unit.MINUTE: 2,
+    Unit.HOUR: 3,
+    Unit.DAY: 4,
+    Unit.WEEK: 5,
+    Unit.MONTH: 6,
+    Unit.YEAR: 7
+}
