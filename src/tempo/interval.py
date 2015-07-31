9dd71ea3ba0af3ef6406155ea969c94a7851bf22
Andrew Pashkin <andrew.pashkin@gmx.co.uk>
2015-08-01 00:18:09 +0300

    Add overlap and combine operations for Interval

--- src/tempo/interval.py
+++ src/tempo/interval.py
@@ -84,3 +84,97 @@ class Interval(object):
 
     def __le__(self, other):
         return self.start >= other.start and self.stop <= other.stop
+
+    def isoverlap(self, other):
+        """Is this interval overlaps with other one?"""
+        if other is EmptyInterval:
+            return False
+        return not (self.stop < other.start or self.start > other.stop)
+
+    def overlap(self, other):
+        """Returns a new instance of Interval, that represents overlap
+        between this interval and a given one.
+
+        If intervals does not overlap, `EmptyInterval` is returned.
+        """
+        if not self.isoverlap(other):
+            return EmptyInterval
+        elif self <= other:
+            return self.__class__(other.start, other.stop)
+        elif self > other:
+            return self.__class__(self.start, self.stop)
+        else:
+            _, start, stop, _ = sorted((self.start, self.stop,
+                                        other.start, other.stop))
+            return self.__class__(start, stop)
+
+    def combine(self, other):
+        """If two intervals intersect, returns a new interval,
+        that cover space of both. Otherwise - returns `EmptyInterval`.
+        """
+        if not self.isoverlap(other):
+            return EmptyInterval
+
+        start, _, _, stop = sorted((self.start, self.stop,
+                                    other.start, other.stop))
+
+        return self.__class__(start, stop)
+
+
+class EmptyIntervalType(Interval):
+
+    __slots__ = ()
+
+    def __new__(cls, *args, **kwargs):
+        global EmptyInterval
+
+        if EmptyInterval is None:
+            return super(EmptyIntervalType, cls).__new__(cls, *args, **kwargs)
+        else:
+            raise TypeError('Can not create new instances.')
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return 'EmptyInterval'
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        if isinstance(other, type(self)):
+            return True
+        else:
+            return False
+
+    def __hash__(self):
+        return 185453665
+
+    def __contains__(self, item):
+        return False
+
+    def __gt__(self, other):
+        return False
+
+    def __ge__(self, other):
+        return self.__eq__(other)
+
+    def __lt__(self, other):
+        return not self.__eq__(other)
+
+    def __le__(self, other):
+        return True
+
+    def isoverlap(self, other):
+        return False
+
+    def overlap(self, other):
+        return self
+
+    def combine(self, other):
+        return other
+
+
+EmptyInterval = None
+EmptyInterval = EmptyIntervalType()
--- tests/test_interval.py
+++ tests/test_interval.py
@@ -4,7 +4,7 @@ from decimal import Decimal
 
 import pytest
 
-from tempo.interval import Interval
+from tempo.interval import Interval, EmptyInterval
 
 
 @pytest.mark.parametrize('interval, item, expected', [
@@ -80,3 +80,54 @@ def test_lt(first, second, expected):
 def test_le(first, second, expected):
     """Reverse included-or-equal test."""
     assert (first <= second) == expected
+
+
+@pytest.mark.parametrize('first, second, expected', [
+    (EmptyInterval, EmptyInterval, True),
+    (EmptyInterval, Interval(1, 10), False),
+    (EmptyInterval, Interval(0, 0), False),
+])
+def test_empty_interval_eq(first, second, expected):
+    """Cases for equality test of EmptyInterval"""
+    assert (first == second) == expected
+
+
+def test_empty_interval_is():
+    """EmptyInterval is EmptyInterval"""
+    assert EmptyInterval is EmptyInterval
+
+
+@pytest.mark.parametrize('first, second, expected', [
+    (Interval(1, 10), Interval(5, 15), True),
+    (Interval(1, 10), Interval(15, 25), False),
+    (Interval(1, 15), Interval(5, 10), True),
+    (Interval(1, 15), Interval(5, 10), True),
+    (EmptyInterval, Interval(5, 10), False),
+])
+def test_isoverlap(first, second, expected):
+    """Cases for overlapping test."""
+    assert first.isoverlap(second) == expected
+    assert second.isoverlap(first) == expected
+
+
+@pytest.mark.parametrize('first, second, expected', [
+    (Interval(1, 10), Interval(5, 15), Interval(5, 10)),
+    (Interval(5, 10), Interval(1, 20), Interval(1, 20)),
+    (Interval(1, 10), Interval(20, 40), EmptyInterval),
+    (Interval(1, 10), EmptyInterval, EmptyInterval),
+])
+def test_overlap(first, second, expected):
+    """Cases for overlap."""
+    assert (first.overlap(second)) == expected
+    assert (second.overlap(first)) == expected
+
+
+@pytest.mark.parametrize('first, second, expected', [
+    (Interval(1, 10), Interval(5, 15), Interval(1, 15)),
+    (Interval(1, 15), Interval(5, 10), Interval(1, 15)),
+    (Interval(1, 10), Interval(15, 25), EmptyInterval),
+])
+def test_combine(first, second, expected):
+    """Cases for combine."""
+    assert (first.combine(second)) == expected
+    assert (second.combine(first)) == expected
