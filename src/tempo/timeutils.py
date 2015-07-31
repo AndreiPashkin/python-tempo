0746f986ad3244e2149375856c4ca76fc42db49e
Andrew Pashkin <andrew.pashkin@gmx.co.uk>
2015-08-01 00:41:34 +0300

    Refactor floor function with elif's

--- src/tempo/timeutils.py
+++ src/tempo/timeutils.py
@@ -12,47 +12,6 @@ from tempo.unit import (Unit, SECONDS_IN_MINUTE, SECONDS_IN_HOUR,
                         DAYS_IN_COMMON_YEAR)
 
 
-def _floor_by_second(datetime):
-    return datetime.replace(microsecond=0)
-
-
-def _floor_by_minute(datetime):
-    return datetime.replace(second=0, microsecond=0)
-
-
-def _floor_by_hour(datetime):
-    return datetime.replace(minute=0, second=0, microsecond=0)
-
-
-def _floor_by_day(datetime):
-    return datetime.replace(hour=0, minute=0, second=0, microsecond=0)
-
-
-def _floor_by_week(datetime):
-    datetime = _floor_by_day(datetime)
-    return datetime - dt.timedelta(days=datetime.weekday())
-
-
-def _floor_by_month(datetime):
-    return datetime.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
-
-
-def _floor_by_year(datetime):
-    return datetime.replace(month=1, day=1, hour=0, minute=0, second=0,
-                            microsecond=0)
-
-
-FLOOR_FNS = {
-    Unit.SECOND: _floor_by_second,
-    Unit.MINUTE: _floor_by_minute,
-    Unit.HOUR: _floor_by_hour,
-    Unit.DAY: _floor_by_day,
-    Unit.WEEK: _floor_by_week,
-    Unit.MONTH: _floor_by_month,
-    Unit.YEAR: _floor_by_year
-}
-
-
 def floor(datetime, unit):
     """Floors given datetime to closest 'unit'.
 
@@ -75,9 +34,24 @@ def floor(datetime, unit):
     ... datetime.datetime(2014, 10, 15, 5, 0, 0)
 
     """
-    try:
-        return FLOOR_FNS[unit](datetime)
-    except KeyError:
+    if unit == Unit.SECOND:
+        return datetime.replace(microsecond=0)
+    elif unit == Unit.MINUTE:
+        return datetime.replace(second=0, microsecond=0)
+    elif unit == Unit.HOUR:
+        return datetime.replace(minute=0, second=0, microsecond=0)
+    elif unit == Unit.DAY:
+        return datetime.replace(hour=0, minute=0, second=0, microsecond=0)
+    elif unit == Unit.WEEK:
+        return (datetime.replace(hour=0, minute=0, second=0, microsecond=0) -
+                dt.timedelta(days=datetime.weekday()))
+    elif unit == Unit.MONTH:
+        return datetime.replace(day=1, hour=0, minute=0, second=0,
+                                microsecond=0)
+    elif unit == Unit.YEAR:
+        return datetime.replace(month=1, day=1, hour=0, minute=0, second=0,
+                                microsecond=0)
+    else:
         raise ValueError
 
 
