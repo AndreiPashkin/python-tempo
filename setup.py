a61e53c6e60ef6d7df364b894d2c717955f036cf
Andrew Pashkin <andrew.pashkin@gmx.co.uk>
2015-10-24 16:32:51 +0300

    Set version to 0.1.0.dev0

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def read_requirements(filename):
 
 setup(
     name="python-tempo",
-    version="0.0.1a",
+    version="0.1.0dev0",
     author="Andrew Pashkin",
     author_email="andrew.pashkin@gmx.co.uk",
     license="BSD",
